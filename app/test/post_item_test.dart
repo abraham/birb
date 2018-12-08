8ea25bb278a641522cad90a8fa7cb1b3479d3534
Abraham Williams <4braham@gmail.com>
2018-12-08 17:08:58 -0600

    Fix test description

--- app/test/post_item_test.dart
+++ app/test/post_item_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Renders list of posts', (WidgetTester tester) async {
+  testWidgets('Renders a post', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MaterialApp(
       home: PostItem(),
